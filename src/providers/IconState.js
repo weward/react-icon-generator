8d850196fd8e4c419aa6ff7c7345bd383f66de41
Roland Edward Santos <dev.weward@gmail.com>
2020-09-16 17:10:13 +0800

    IG-4 Upload and download Image

--- src/providers/IconState.js
+++ src/providers/IconState.js
@@ -1,5 +1,6 @@
 import React, { createContext, useReducer } from 'react'
 import IconReducer from '../reducers/IconReducer'
+import axios from 'axios'
 
 const initialState = {
     file: {
@@ -28,9 +29,46 @@ export const IconProvider = (props) => {
         })
     }
 
+    function processImage(data) {
+        return new Promise((resolve, reject) => {
+            // proceess image
+            axios({
+                method: 'POST', 
+                url: `${process.env.REACT_APP_API_URL}/generate-icons`,
+                data
+            }).then((res) => {
+                // download
+                axios({
+                    method: 'POST',
+                    url: `${process.env.REACT_APP_API_URL}/download`,
+                    responseType: 'blob',
+                    data: {
+                        dir: res.data.dir
+                    }
+                }).then((resd) => {
+                    const url = window.URL.createObjectURL(new Blob([resd.data]));
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.setAttribute('download', 'weward-icons.zip');
+                    document.body.appendChild(link);
+                    link.click();
+
+                    resolve()
+                }).catch(() => {
+                    reject('Download Failed.')
+                })
+            }).catch(() => {
+                reject('Processing Failed.')
+            })
+        });
+    }
+
     return <IconContext.Provider value={{
+        // STATE
         file: state.file,
-        greet
+        // ACTIONS
+        greet,
+        processImage,
     }}>
         { props.children }
     </IconContext.Provider>
